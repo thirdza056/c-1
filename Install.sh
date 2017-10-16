95c55494cb6557ded472ec6d74908bc35e998c15
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2017-10-16 10:17:24 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -39,6 +39,9 @@ cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 # Update
 apt-get update
 
+# Install Webserver
+apt-get -y install nginx
+
 # Install Essential Package
 apt-get -y install nano iptables dnsutils openvpn screen whois ngrep unzip unrar
 
@@ -50,67 +53,68 @@ echo "clear" >> .profile
 echo "screenfetch" >> .profile
 
 # Install Webserver
-apt-get -y install nginx php5-fpm php5-cli
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-cat > /etc/nginx/nginx.conf <<END
+cat > /etc/nginx/nginx.conf <<END3
 user www-data;
-
-worker_processes 2;
+worker_processes 1;
 pid /var/run/nginx.pid;
 events {
 	multi_accept on;
-        worker_connections 1024;
+  worker_connections 1024;
 }
 http {
+	gzip on;
+	gzip_vary on;
+	gzip_comp_level 5;
+	gzip_types    text/plain application/x-javascript text/xml text/css;
 	autoindex on;
-        sendfile on;
-        tcp_nopush on;
-        tcp_nodelay on;
-        keepalive_timeout 65;
-        types_hash_max_size 2048;
-        server_tokens off;
-        include /etc/nginx/mime.types;
-        default_type application/octet-stream;
-        access_log /var/log/nginx/access.log;
-        error_log /var/log/nginx/error.log;
-        client_max_body_size 32M;
+  sendfile on;
+  tcp_nopush on;
+  tcp_nodelay on;
+  keepalive_timeout 65;
+  types_hash_max_size 2048;
+  server_tokens off;
+  include /etc/nginx/mime.types;
+  default_type application/octet-stream;
+  access_log /var/log/nginx/access.log;
+  error_log /var/log/nginx/error.log;
+  client_max_body_size 32M;
 	client_header_buffer_size 8m;
 	large_client_header_buffers 8 8m;
-
 	fastcgi_buffer_size 8m;
 	fastcgi_buffers 8 8m;
-
 	fastcgi_read_timeout 600;
-
-        include /etc/nginx/conf.d/*.conf;
+  include /etc/nginx/conf.d/*.conf;
 }
-END
+END3
 mkdir -p /home/vps/public_html
-echo "<pre>Source by Mnm Ami</pre>" > /home/vps/public_html/index.html
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-cat > /etc/nginx/conf.d/vps.conf <<END
+echo "<pre>Source by Mnm Ami | Donate at TrueMoney Wallet 082-038-2600</pre>" > /home/vps/public_html/index.html
+echo "<?phpinfo(); ?>" > /home/vps/public_html/info.php
+args='$args'
+uri='$uri'
+document_root='$document_root'
+fastcgi_script_name='$fastcgi_script_name'
+cat > /etc/nginx/conf.d/vps.conf <<END4
 server {
-    listen       80;
-    server_name  127.0.0.1 localhost;
-    access_log /var/log/nginx/vps-access.log;
-    error_log /var/log/nginx/vps-error.log error;
-    root   /home/vps/public_html;
-    location / {
-        index  index.html index.htm index.php;
-	try_files $uri $uri/ /index.php?$args;
-    }
-    location ~ \.php$ {
-        include /etc/nginx/fastcgi_params;
-        fastcgi_pass  127.0.0.1:9000;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    }
+  listen       80;
+  server_name  127.0.0.1 localhost;
+  access_log /var/log/nginx/vps-access.log;
+  error_log /var/log/nginx/vps-error.log error;
+  root   /home/vps/public_html;
+  location / {
+    index  index.html index.htm index.php;
+    try_files $uri $uri/ /index.php?$args;
+  }
+  location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass  127.0.0.1:9000;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  }
 }
-END
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
+END4
 service nginx restart
 
 # Install Vnstat
@@ -255,7 +259,6 @@ service cron restart
 service ssh restart
 service dropbear restart
 service vnstat restart
-service php5-fpm restart
 service squid3 restart
 rm -rf ~/.bash_history && history -c
 
