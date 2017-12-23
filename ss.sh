b35f7573f5f9e8854744d114beec021333ea99be
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2017-12-23 19:57:32 +0700

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,23 +1,136 @@
 #!/bin/bash
 
-apt-get -y install ufw
-apt-get -y install sudo
-sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
-sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
-sudo yes | ufw enable
+# Initialization var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
 
-apt-get -y install php5-fpm php5-cli
+# Go to root
+cd
+
+# Disable ipv6
+#echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+#sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# Install Essential Package
+apt-get -y install nginx php5-fpm php5-cli iptables openvpn wget curl
+
+# Update
+apt-get update
+
+# Set Timezone Thailand GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+
+# Set Locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# Set repo
+cat > /etc/apt/sources.list <<END
+deb http://mirrors.digitalocean.com/debian jessie main
+deb-src http://mirrors.digitalocean.com/debian jessie main
+
+deb http://security.debian.org/ jessie/updates main
+deb-src http://security.debian.org/ jessie/updates main
+
+deb http://mirrors.digitalocean.com/debian jessie-updates main
+deb-src http://mirrors.digitalocean.com/debian jessie-updates main
+
+deb http://cdn.debian.net/debian wheezy main contrib non-free
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb http://packages.dotdeb.org wheezy all
+END
+wget "https://raw.githubusercontent.com/nwqionnwkn/OPENEXTRA/master/Config/dotdeb.gpg"
+wget "https://raw.githubusercontent.com/nwqionnwkn/OPENEXTRA/master/Config/jcameron-key.asc"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
+
+# Update
+apt-get update
+
+# Install Screenfetch
+cd
+wget -O /usr/bin/screenfetch "https://raw.githubusercontent.com/nwqionnwkn/OPENEXTRA/master/Config/screenfetch"
+chmod +x /usr/bin/screenfetch
+echo "clear" >> .profile
+echo "screenfetch" >> .profile
+
+# Install Webserver
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
+cat > /etc/nginx/nginx.conf <<END3
+user www-data;
+worker_processes 1;
+pid /var/run/nginx.pid;
+events {
+	multi_accept on;
+  worker_connections 1024;
+}
+http {
+	gzip on;
+	gzip_vary on;
+	gzip_comp_level 5;
+	gzip_types    text/plain application/x-javascript text/xml text/css;
+	autoindex on;
+  sendfile on;
+  tcp_nopush on;
+  tcp_nodelay on;
+  keepalive_timeout 65;
+  types_hash_max_size 2048;
+  server_tokens off;
+  include /etc/nginx/mime.types;
+  default_type application/octet-stream;
+  access_log /var/log/nginx/access.log;
+  error_log /var/log/nginx/error.log;
+  client_max_body_size 32M;
+	client_header_buffer_size 8m;
+	large_client_header_buffers 8 8m;
+	fastcgi_buffer_size 8m;
+	fastcgi_buffers 8 8m;
+	fastcgi_read_timeout 600;
+  include /etc/nginx/conf.d/*.conf;
+}
+END3
+mkdir -p /home/vps/public_html
+echo "<pre>Source by Mnm Ami | Donate at TrueMoney Wallet 082-038-2600</pre>" > /home/vps/public_html/index.html
+echo "<?phpinfo(); ?>" > /home/vps/public_html/info.php
+args='$args'
+uri='$uri'
+document_root='$document_root'
+fastcgi_script_name='$fastcgi_script_name'
+cat > /etc/nginx/conf.d/vps.conf <<END4
+server {
+  listen       85;
+  server_name  127.0.0.1 localhost;
+  access_log /var/log/nginx/vps-access.log;
+  error_log /var/log/nginx/vps-error.log error;
+  root   /home/vps/public_html;
+  location / {
+    index  index.html index.htm index.php;
+    try_files $uri $uri/ /index.php?$args;
+  }
+  location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass  127.0.0.1:9000;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+  }
+}
+END4
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 /etc/init.d/nginx restart
 /etc/init.d/php5-fpm restart
 
+# Install Vnstat
+cd
+apt-get -y install vnstat
 vnstat -u -i eth0
-sudo chown -R vnstat:vnstat /var/lib/vnstat
-service vnstat restart
+chown -R vnstat:vnstat /var/lib/vnstat
+/etc/init.d/vnstat restart
 
+# Install Vnstat GUI
 cd /home/vps/public_html/
 wget https://github.com/nwqionnwkn/OPENEXTRA/raw/master/Config/vnstat_php_frontend-1.5.1.tar.gz
 tar xf vnstat_php_frontend-1.5.1.tar.gz
@@ -29,14 +142,136 @@ sed -i "s/\$language = 'th';/\$language = 'en';/g" config.php
 sed -i 's/Internal/Internet/g' config.php
 sed -i '/IPv6/d' config.php
 
+# Install OpenVPN
+wget -O /etc/openvpn/openvpn.tar "https://github.com/nwqionnwkn/OPENEXTRA/raw/master/Config/openvpn.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+cat > /etc/openvpn/1194.conf <<END
+port 1194
+proto tcp
+dev tun
+
+ca /etc/openvpn/keys/ca.crt
+dh /etc/openvpn/keys/dh1024.pem
+cert /etc/openvpn/keys/server.crt
+key /etc/openvpn/keys/server.key
+
+plugin /usr/lib/openvpn/openvpn-auth-pam.so /etc/pam.d/login
+client-cert-not-required
+username-as-common-name
+
+server 192.168.100.0  255.255.255.0
+push "redirect-gateway def1"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+
+cipher none
+comp-lzo
+
+keepalive 5 30
+
+persist-key
+persist-tun
+client-to-client
+status log.log
+verb 3
+mute 10
+END
+/etc/init.d/openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables_new.conf
+cat > /etc/network/if-up.d/iptables <<END
+#!/bin/sh
+iptables-restore < /etc/iptables_new.conf
+END
+chmod +x /etc/network/if-up.d/iptables
+/etc/init.d/openvpn restart
+
+# Setting Port SSH
 cd
-chown -R www-data:www-data /home/vps/public_html
-/etc/init.d/nginx restart
-/etc/init.d/php5-fpm restart
-service openvpn restart
-service cron restart
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# Install Dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 80"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
-service vnstat restart
-service squid3 restart
-rm -rf ~/.bash_history && history -c
+
+# Install Squid3
+cd
+apt-get -y install squid3
+cat > /etc/squid3/squid.conf <<END
+acl manager proto cache_object
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+http_access allow SSH
+http_access allow manager localhost
+http_access deny manager
+http_access allow localhost
+http_access deny all
+http_port 8080
+coredump_dir /var/spool/squid3
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname openextra.net
+END
+sed -i $MYIP2 /etc/squid3/squid.conf;
+
+# Install Script
+cd /usr/local/bin
+wget https://raw.githubusercontent.com/nwqionnwkn/OPENEXTRA/master/Config/menu
+wget https://raw.githubusercontent.com/nwqionnwkn/OPENEXTRA/master/Config/speedtest
+chmod +x menu
+chmod +x speedtest
+echo ""
+echo "..... Installing 98% ...restarting service."
+
+# Finishing
+cd
+/etc/init.d/nginx restart
+/etc/init.d/php5-fpm restart
+/etc/init.d/openvpn restart
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/vnstat restart
+/etc/init.d/squid3 restart
+
+# info
+clear
+echo "====================================================="
+echo ""
+echo " - OpenVPN  : TCP Port 1194"
+echo " - OpenSSH  : Port 22, 143"
+echo " - Dropbear : Port 80, 443"
+echo " - Squid3   : Port 8080"
+echo ""
+echo "====================================================="
+echo "หลังจากติดตั้งสำเร็จ... กรุณาพิมพ์คำสั่ง menu เพื่อไปยังขั้นตอนถัดไป"
+echo "====================================================="
+echo "-------- Script by Mnm Ami"
+cd
+rm -f /root/Install.sh
