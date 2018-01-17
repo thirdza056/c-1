4c5702c79187412b17757cd828483ae273900551
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-17 16:53:43 +0700

    Update Pritunl.sh

--- Pritunl.sh
+++ Pritunl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dist=`grep Codename lsb_release -c | awk -F '=' '{print $2}'`
+dist=`grep VERSION_ID /etc/*-release | awk -F '=' '{print $2}'`
 
-if [ "$dist" == "jessie" ]; then
+if [ "$dist" == "8" ]; then
   echo "8"
-elif [ "$dist" == "trusty" ]; then
+elif [ "$dist" == "14.04" ]; then
   echo "14"
-elif [ "$dist" == "xenial" ]; then
+elif [ "$dist" == "16.04" ]; then
   echo "16"
 else
 echo "0000"
