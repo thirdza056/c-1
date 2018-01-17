5cd95daa1330d2786215dc83f0564ace8e3a41f3
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-17 15:57:26 +0700

    Update Pritunl.sh

--- Pritunl.sh
+++ Pritunl.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+function add_vapor_apt() {
+    eval "$(cat /etc/lsb-release)"
 
-if [[ "$DISTRIB_CODENAME" != "trusty" ]]; then
-echo "....."
-fi
+    if [[ "$DISTRIB_CODENAME" != "xenial" && "$DISTRIB_CODENAME" != "yakkety" && "$DISTRIB_CODENAME" != "trusty" ]];
+    then
+        echo "Only Ubuntu 14.04, 16.04, and 16.10 are supported."
+        echo "You are running $DISTRIB_RELEASE ($DISTRIB_CODENAME) [`uname`]"
+        return 1;
+    fi
+        echo "....."
+        exit
+}
 
-echo "+++++"
+add_vapor_apt
