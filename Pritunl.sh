f730c9bda43137052d20539729c4f080d24ce40e
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-17 18:54:55 +0700

    Update Pritunl.sh

--- Pritunl.sh
+++ Pritunl.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-if [[ "$OS" = 'debian' ]]; then
+if [[ -e /etc/debian_version ]]; then
+        OS="debian"
+	VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
+	IPTABLES='/etc/iptables/iptables.rules'
+	SYSCTL='/etc/sysctl.conf'
+
+    if [[ "$OS" = 'debian' ]]; then
 
 		# Debian 7
 		if [[ "$VERSION_ID" = 'VERSION_ID="7"' ]]; then
 			echo "7"
 		fi
+		
 		# Debian 8
 		if [[ "$VERSION_ID" = 'VERSION_ID="8"' ]]; then
 			echo "8"
 		fi
 
-elif [[ "$OS" = 'ubuntu' ]]; then
-
 		# Ubuntu 14.04
 		if [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
 			echo "14.04"
 		fi
+		
 		# Ubuntu 16.04
 		if [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
 			echo "16.04"
 		fi
+    fi
 fi
