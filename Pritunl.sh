bf1e72284ef335eaf1274c9a4f57034b6f0442a9
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-17 18:33:47 +0700

    Update Pritunl.sh

--- Pritunl.sh
+++ Pritunl.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if platform?("ubuntu")
-echo "ubuntu..."
-elif platform?("debian")
-echo "Debian..."
-else
-echo "....."
-if
+
+		# Debian 7
+		if [[ "$VERSION_ID" = 'VERSION_ID="7"' ]]; then
+			echo "7"
+		fi
+		# Debian 8
+		if [[ "$VERSION_ID" = 'VERSION_ID="8"' ]]; then
+			echo "8"
+		fi
+		# Ubuntu 14.04
+		if [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
+			echo "14.04"
+		fi
+		# Ubuntu 16.04
+		if [[ "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
+			echo "16.04"
+		fi
