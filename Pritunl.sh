c41cea38783cfddd51d452daa25508e345085afa
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-17 16:06:49 +0700

    Update Pritunl.sh

--- Pritunl.sh
+++ Pritunl.sh
@@ -2,14 +2,14 @@
 
 if [[ ${DISTRIB_CODENAME} == "trusty" ]]; then
     echo "=== 14.04"
-    exit
     
 elif [[ ${DISTRIB_CODENAME} == "xenial" ]]; then
     echo "=== 16.04"
-    exit
     
 elif [[ ${DISTRIB_CODENAME} == "jessie" ]]; then
     echo "=== 8"
-    exit
+    
+else
+    echo "พัง...!!"
     
 fi
