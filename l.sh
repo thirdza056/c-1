61609037fbe1f452c8a5af79e9df3132223dc8fd
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-20 19:11:58 +0700

    Create l.sh

---
+++ l.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+			while [[ $CONTINUE != "1" && $CONTINUE != "2" ]]; do
+
+				echo ""
+				echo "คุณต้องการติดตั้ง Squid Proxy หรือไม่ ?"
+				read -p "You Need Install Squid Proxy or Not ? :" -e -i 1 CONTINUE
+
+			done
+
+				if [[ "$CONTINUE" = "2" ]]; then
+				echo "บายยย"
+        exit
+				fi
+       
+       echo "1 ไม่บาย"
+       exit
