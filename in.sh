25a9ceb889f55db237eeba190028dddf2c6af9f4
nwqionnwkn <32827182+nwqionnwkn@users.noreply.github.com>
2018-01-22 22:32:40 +0700

    Update in.sh

--- in.sh
+++ in.sh
@@ -420,9 +420,12 @@ verb 3" > /etc/openvpn/client-common.txt
 
 apt-get -y install squid3
 cat > /etc/squid3/squid.conf <<END
-acl manager proto cache_object
+http_port 8080
 acl localhost src 127.0.0.1/32 ::1
 acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl localnet src 10.0.0.0/8
+acl localnet src 172.16.0.0/12
+acl localnet src 192.168.0.0/16
 acl SSL_ports port 443
 acl Safe_ports port 80
 acl Safe_ports port 21
@@ -435,20 +438,18 @@ acl Safe_ports port 488
 acl Safe_ports port 591
 acl Safe_ports port 777
 acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255                 
 http_access allow SSH
-http_access allow manager localhost
-http_access deny manager
+http_access allow localnet
 http_access allow localhost
 http_access deny all
-http_port 8080
-coredump_dir /var/spool/squid3
-refresh_pattern ^ftp: 1440 20% 10080
-refresh_pattern ^gopher: 1440 0% 1440
-refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
-refresh_pattern . 0 20% 4320
-visible_hostname openextra.net
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
 END
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+IP2="s/xxxxxxxxx/$IP/g";
 sed -i $IP2 /etc/squid3/squid.conf;
 service squid3 restart
 
